aadb87665f5ed6e0f5846a19360d7d83ff40c7c8
Eduardo Bernardino <eduardomoura.moura@mail.com>
2020-04-09 10:26:24 -0300

    MT -> Running

--- maquina_turing.py
+++ maquina_turing.py
@@ -5,7 +5,6 @@
 import os
 import numpy
 import time
-import multiprocessing
 
 def esta_comentada(linha):
   if linha[0] == '#':
@@ -67,7 +66,69 @@ class MaquinaTuring:
     self.variaveis.append(self.simbolo_final)
     self.delta = self.extra_delta(delta, self.variaveis)
     print(self.delta)
+  
+  def escrever_fita(self, estado_atual, string_fita, proximo_estado, parada = False):
+    if parada and self.estadoFinal(proximo_estado):
+      status = 'ACEITO'
+    elif parada:
+      status = 'REJEITADO'
+    else:
+      status = 'RODANDO'
+    with open(self.arquivo_fita, 'w+') as handle:
+      handle.write(str(estado_atual) + '\n'
+                    + string_fita + '\n'
+                    + status + '\n'
+                    + str(proximo_estado) + '\n'
+                    + str(self.velocidade))
 
-mt = MaquinaTuring("fita.txt", velocidade = 0.1)
+  def estadoFinal(self, state):
+    if state == self.estado_final:
+      return True
+    return False
+  
+  def get_acao(self, acao, input, index):
+    if not acao:
+      return False
+    if 'R' in acao:
+      proximo_index = index + 1
+      if proximo_index > len(input)-1:
+        input += self.simbolo_final
+      proximo_estado = acao.split('R')[-1]
+      input = input[:index] + acao.split('R')[0] + input[(index+1):]
+    else:
+      proximo_index = index - 1
+      if proximo_index < 0:
+        input = self.simbolo_final + input
+      proximo_estado = acao.split('L')[-1]
+      input = input[:index] + action.split('L')[0] + input[(index+1):]
+    return proximo_index, input, proximo_estado
+  
+  def simular(self, input):
+    if input[-1] != self.simbolo_final:
+      input += self.simbolo_final
+    proximo_estado = self.estado_inicial
+    proximo_index = 0
+    status = "Rodando"
+    for index, simbolo in enumerate(input):
+      self.escrever_fita(index, input, proximo_estado)
+      simbolo = input[proximo_index]
+      try:
+        acao = self.delta[proximo_estado][simbolo]
+      except:
+        self.escrever_fita(index, input, proximo_estado, parada=True)
+        return
+      print(f"String: {input}")
+      print(f"Ação: {acao}")
+      proxima_acao = self.get_acao(acao, input, index)
+      proximo_index, input, proximo_estado = proxima_acao
+      time.sleep(self.velocidade)
+    self.escrever_fita(index, input, proximo_estado, parada=True)
 
-mt.get_config("config.txt")
+
+if __name__ == "__main__":
+  mt = MaquinaTuring("fita.txt", velocidade = 0.1)
+
+  mt.get_config("config.txt")
+
+  mt.simular("aaa") # Deve Aceitar
+  # mt.simular("aaabaabbaaacabbsss") # Deve Rejeitar
