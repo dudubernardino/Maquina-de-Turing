e715ad11f8a89ee9dc7e4f74602bf8fabd37895a
Eduardo Bernardino <eduardomoura.moura@mail.com>
2020-04-14 14:34:09 -0300

    READ-TXT

---
+++ src/ler_arquivo.py
@@ -0,0 +1,30 @@
+class ler_arquivo():
+  def __init__(self, nome_arquivo):
+    self.nome_arquivo = nome_arquivo
+
+  def get_linhas(self):
+    f = open(self.nome_arquivo, 'r')
+    self.config = []
+    self.transicoes = []
+    self.count = 0
+    for linha in f:
+      if "alfabeto" in linha:
+        linha = linha.rstrip()
+        self.alfabeto = list(filter(
+          None, linha.split(':')[1].split(',')
+        ))
+       
+      elif "alfabet0_aux" in linha:
+        linha = linha.rstrip()
+        self.alfabeto_aux = list(filter(
+          None, linha.split(':')[1].split(',')
+        ))
+      elif "trans_q" in linha:
+        linha = linha.rstrip()
+        self.transicoes.append(list(filter(
+            None, linha.split(':')[1].split('[]')
+          )))
+            
+
+
+#globals()["trans_q" + str(i)] = arquivo.transicoes[i]
--- src/maquina1.py
+++ src/maquina1.py
@@ -1,4 +1,15 @@
 from turing import *
+from ler_arquivo import *
+
+arquivo = ler_arquivo('src/config.txt')
+arquivo.get_linhas()
+
+# Separa as transições em variáveis
+for i in range(len(arquivo.transicoes)):
+    for transicao in arquivo.transicoes[i]:
+      globals()["trans_q" + str(i)] = transicao
+
+print(trans_q0)
 
 '''
 Maquina de Turing 1
@@ -8,8 +19,9 @@ L = (a^n)(b^n)
 DIR = True
 ESQ = False
 
-alfabeto = ['a','b']
-alfabeto_aux = ['A','B']
+alfabeto = arquivo.alfabeto
+alfabeto_aux = arquivo.alfabeto_aux
+
 
 trans_q0 = [["#","#",DIR,"q0"],['a','A',DIR,"q1"],['B','B',DIR,"q3"],[' ',' ',DIR,"q4"]]
 trans_q1 = [['b','B',ESQ,"q2"],['a','a',DIR,"q1"],['B','B',DIR,"q1"]]
@@ -23,8 +35,8 @@ q2 = estado("q2",trans_q2)
 q3 = estado("q3",trans_q3)
 q4 = estado("q4",trans_q4,is_final=True)
 
-turing = turing(alfabeto,[q0,q1,q2,q3,q4],"q0",alfabeto_aux,'#',' ')
-turing.aceita("ab")
-turing.aceita("aabb")
-turing.aceita("aaab")
-turing.aceita("aaaabbbb")
+# turing = turing(alfabeto,[q0,q1,q2,q3,q4],"q0",alfabeto_aux,'#',' ')
+# turing.aceita("ab")
+# turing.aceita("aabb")
+# turing.aceita("aaab")
+# turing.aceita("aaaabbbb")
