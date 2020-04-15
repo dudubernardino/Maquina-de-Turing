1884fd764796c8b15e7a33c154019165289327f5
Eduardo Bernardino <eduardomoura.moura@mail.com>
2020-04-15 12:17:14 -0300

    TRANSICAO -> TESTS

--- src/maquina1.py
+++ src/maquina1.py
@@ -20,7 +20,7 @@ for i in range(len(arquivo.transicoes)):
         if "DIR" in direcao:
           transicao[2] = DIR
         elif "ESQ" in direcao:
-          transicao[2] = ESQ       
+          transicao[2] = ESQ   
       globals()["trans_q" + str(i)].append(transicao)
     
 print(trans_q0)
@@ -35,20 +35,20 @@ estado_inicial = arquivo.estado_inicial[0]
 simbolo_inicial = arquivo.simbolo_inicial[0]
 
 
-trans_q0 = [["#","#",DIR,"q0"],['a','A',DIR,"q1"],['B','B',DIR,"q3"],[' ',' ',DIR,"q4"]]
+transicao_q0 = [[trans_q0[0][0],trans_q0[0][1],trans_q0[0][2], 'q0'],['a','A',DIR,"q1"],['B','B',DIR,"q3"],[' ',' ',DIR,"q4"]]
 trans_q1 = [['b','B',ESQ,"q2"],['a','a',DIR,"q1"],['B','B',DIR,"q1"]]
 trans_q2 = [['A','A',DIR,"q0"],['a','a',ESQ,"q2"],['B','B',ESQ,"q2"]]
 trans_q3 = [['B','B',DIR,"q3"],[' ',' ',DIR,"q4"]]
 trans_q4 = []
 
-q0 = estado("q0",trans_q0)
+q0 = estado("q0",transicao_q0)
 q1 = estado("q1",trans_q1)
 q2 = estado("q2",trans_q2)
 q3 = estado("q3",trans_q3)
 q4 = estado("q4",trans_q4,is_final=True)
 
-# turing = turing(alfabeto,[q0,q1,q2,q3,q4],'q0',alfabeto_aux,simbolo_inicial,' ')
-# turing.aceita("ab")
+turing = turing(alfabeto,[q0,q1,q2,q3,q4],'q0',alfabeto_aux,simbolo_inicial,' ')
+turing.aceita("ab")
 # turing.aceita("aabb")
 # turing.aceita("aaab")
 # turing.aceita("aaaabbbb")
