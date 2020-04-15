b12963a4b79c434b5f4488e201595ce0f0da390c
Eduardo Bernardino <eduardomoura.moura@mail.com>
2020-04-15 11:00:07 -0300

    TRANSICAO -> NEW CODE -> AJUSTED

--- src/maquina1.py
+++ src/maquina1.py
@@ -8,21 +8,24 @@ arquivo.get_linhas()
 DIR = True
 ESQ = False
 
+# Organizando Transições
 nova_transicao = []
 for i in range(len(arquivo.transicoes)):
   for transicao in arquivo.transicoes[i]:
-    globals()["trans_q" + str(i)] = transicao.replace('[', '').replace(']', '').split(';')
-    for direcao in globals()["trans_q" + str(i)]:
+    globals()["trans_q" + str(i)] = []
+    nova_transicao = transicao.replace('[', '').replace(']', '').split(';')
+    for direcao in nova_transicao:
       transicao = direcao.split(',')
       for direcao in transicao:
         if "DIR" in direcao:
           transicao[2] = DIR
         elif "ESQ" in direcao:
-          transicao[2] = DIR = ESQ
-      nova_transicao.append(transicao)
-
-print(nova_transicao[0])
-
+          transicao[2] = ESQ       
+      globals()["trans_q" + str(i)].append(transicao)
+    
+print(trans_q0)
+print(trans_q1)
+print(trans_q2)
 
 
 alfabeto = arquivo.alfabeto
@@ -37,7 +40,6 @@ trans_q1 = [['b','B',ESQ,"q2"],['a','a',DIR,"q1"],['B','B',DIR,"q1"]]
 trans_q2 = [['A','A',DIR,"q0"],['a','a',ESQ,"q2"],['B','B',ESQ,"q2"]]
 trans_q3 = [['B','B',DIR,"q3"],[' ',' ',DIR,"q4"]]
 trans_q4 = []
-
 
 q0 = estado("q0",trans_q0)
 q1 = estado("q1",trans_q1)
